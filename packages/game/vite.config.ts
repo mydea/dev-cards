5a02b9fe9fdb792ba80e74f9f8cddda2d50b2c03
Francesco Novy <francesco.novy@sentry.io>
2025-08-21 15:33:11 +0200

    add sentry vite plugin

--- packages/game/vite.config.ts
+++ packages/game/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: 'francescos-organization',
+      project: 'draw-it-play-it-ship-it-game',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
